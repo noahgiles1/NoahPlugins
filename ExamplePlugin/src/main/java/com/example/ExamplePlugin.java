fcd7ddc91d846b6468e77b3ac36efbfedb9977c2
noahgiles1 <44266155+noahgiles1@users.noreply.github.com>
2023-08-30 19:29:24 +0100

    init commit - make it maven
    - Utils for sending clicks

--- ExamplePlugin/src/main/java/com/example/ExamplePlugin.java
+++ ExamplePlugin/src/main/java/com/example/ExamplePlugin.java
@@ -6,11 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.Point;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import com.noah.runelite.utils.Utils;
+
+import java.awt.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @PluginDescriptor(
@@ -24,16 +31,23 @@ public class ExamplePlugin extends Plugin
 	@Inject
 	private ExampleConfig config;
 
+	@Inject
+	private Utils extUtils;
+
+	private ExecutorService executorService;
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("Example started!");
+		executorService = Executors.newSingleThreadExecutor();
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		log.info("Example stopped!");
+		executorService.shutdown();
 	}
 
 	@Subscribe
@@ -42,6 +56,26 @@ public class ExamplePlugin extends Plugin
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick gameTick)
+	{
+		if (client.getGameState() == GameState.LOGGED_IN)
+		{
+			executorService.submit(() -> {
+				log.info("Executing on executor thread separate to runelite client");
+
+				Rectangle rect = client.getLocalPlayer().getCanvasTilePoly().getBounds();
+				log.info("Player tile location: " + client.getLocalPlayer().getCanvasTilePoly().getBounds());
+
+				Point point = extUtils.getClickPoint(rect);
+				log.info("Random point in object: " + point);
+
+				log.info("Sending click at random point");
+				//extUtils.click(client.getMouseCanvasPosition());
+			});
 		}
 	}
 
---
+++ Utils/src/main/java/com/noah/runelite/utils/Utils.java
@@ -0,0 +1,136 @@
+package com.noah.runelite.utils;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.Point;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "com/extutils",
+        hidden = true
+)
+@Slf4j
+@SuppressWarnings("unused")
+@Singleton
+public class Utils extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Override
+    protected void startUp()
+    {
+
+    }
+
+    @Override
+    protected void shutDown()
+    {
+
+    }
+
+    public int[] stringToIntArray(String string)
+    {
+        return Arrays.stream(string.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+    }
+
+    /**
+     * This method must be called on a new
+     * thread, if you try to call it on
+     * {@link net.runelite.client.callback.ClientThread}
+     * it will result in a crash/desynced thread.
+     */
+    public void typeString(String string)
+    {
+        assert !client.isClientThread();
+
+        for (char c : string.toCharArray())
+        {
+            pressKey(c);
+        }
+    }
+
+    public void pressKey(char key)
+    {
+        keyEvent(401, key);
+        keyEvent(402, key);
+        keyEvent(400, key);
+    }
+
+    private void keyEvent(int id, char key)
+    {
+        KeyEvent e = new KeyEvent(
+                client.getCanvas(), id, System.currentTimeMillis(),
+                0, KeyEvent.VK_UNDEFINED, key
+        );
+
+        client.getCanvas().dispatchEvent(e);
+    }
+
+    /**
+     * This method must be called on a new
+     * thread, if you try to call it on
+     * {@link net.runelite.client.callback.ClientThread}
+     * it will result in a crash/desynced thread.
+     */
+    public void click(Rectangle rectangle)
+    {
+        assert !client.isClientThread();
+        Point point = getClickPoint(rectangle);
+        click(point);
+    }
+
+    public void click(Point p)
+    {
+        assert !client.isClientThread();
+
+        if (client.isStretchedEnabled())
+        {
+            final Dimension stretched = client.getStretchedDimensions();
+            final Dimension real = client.getRealDimensions();
+            final double width = (stretched.width / real.getWidth());
+            final double height = (stretched.height / real.getHeight());
+            final Point point = new Point((int) (p.getX() * width), (int) (p.getY() * height));
+            mouseEvent(501, point);
+            mouseEvent(502, point);
+            mouseEvent(500, point);
+            return;
+        }
+        mouseEvent(501, p);
+        mouseEvent(502, p);
+        mouseEvent(500, p);
+    }
+
+    public Point getClickPoint(Rectangle rect)
+    {
+        final int x = (int) (rect.getX() + getRandomIntBetweenRange((int) rect.getWidth() / 6 * -1, (int) rect.getWidth() / 6) + rect.getWidth() / 2);
+        final int y = (int) (rect.getY() + getRandomIntBetweenRange((int) rect.getHeight() / 6 * -1, (int) rect.getHeight() / 6) + rect.getHeight() / 2);
+
+        return new Point(x, y);
+    }
+
+    public int getRandomIntBetweenRange(int min, int max)
+    {
+        return (int) ((Math.random() * ((max - min) + 1)) + min);
+    }
+
+    private void mouseEvent(int id, Point point)
+    {
+        MouseEvent e = new MouseEvent(
+                client.getCanvas(), id,
+                System.currentTimeMillis(),
+                0, point.getX(), point.getY(),
+                1, false, 1
+        );
+
+        client.getCanvas().dispatchEvent(e);
+    }
+}
